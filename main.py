2981020b25ec8b31f73989e776f3dd899bccce36
DoggoNako <sashapoun@gmail.com>
2024-03-11 17:44:24 +0100

    functions updated, new endpoints, getting errors [MAIN.py] GET/DELETE/POST methods added/updated, now it returns error and send true informations [DB.py] can now get user by his username, fix a bug where we cannot commit a SQL command, added some try...expect function to have futher explanations on some errors [TODO] Make some endpoints for post and update python/sql functions to add futher comment on errors. Also add mail and password to user (need to do some research)

--- main.py
+++ main.py
@@ -7,37 +7,63 @@ app = Flask(__name__)
 def hello_world():
     return """<h1>Welcome to Bonfire API</h1>
     <p>Nothing is here... but you can try those:</p>
-    <code>GET/DELETE http://localhost:5000/api/user/'user_id'</code>
-    <code>POST http://localhost:5000/api/user/create</code>
+    <code>GET/DELETE http://localhost:5000/api/user/'user_id'</code><br/>
+    <code>POST http://localhost:5000/api/user/register</code><br/>
     <code>GET/DELETE http://localhost:5000/api/post/'post_id'</code>"""
 
-@app.route('/api/user/<int:user_id>', methods=['GET', 'DELETE'])
+#All abort() function return a HTTP Code, but also a HTML Body (bad?)
+
+@app.route('/api/user/<user_id>', methods=['GET', 'DELETE'])
 def user_methods(user_id):
-    if request.method == 'GET':
-        result = getUser(user_id)
-        print(result)
-        if result == None:
-            abort(404, f'{user_id} was not found')
-        else:
-            return {
-                "id": "",
-                "username": "",
-                "following": "",
-                "followed_by": "",
-                "created_at": ""
-            }
-    elif request.method == 'DELETE':
-        #function to remove user from the db
-        return f"user {user_id} succesfuly deleted", 200
+    match request.method:
+        case 'GET':
+            result = getUser(user_id)
+            match result:
+                case 'error': abort(500) 
+                case None: return f"User {user_id} Not Found", 404
+                case 'wrong type': abort(400)
+                case _: return {
+                    "id": result[0],
+                    "username": result[1],
+                    "following": result[2],
+                    "followed_by": result[3],
+                    "created_at": result[5]
+                }
 
-#@app.route('/api/user/create', methods=['POST'])
-#def create_new_user():
+        case 'DELETE':
+            result = deleteUser(user_id)
+            match result:
+                case 'success': return f"User {user_id} Succesfuly Deleted", 200
+                case 'error': abort(500)
+                case 'wrong type': abort(400)
+                    #TODO:
+                    # return "need a user id to delete". I'm not putting both function as getUser 
+                    # because can already have user id with getUser function.
 
-@app.route('/api/post/<int:post_id>', methods=['GET', 'DELETE'])
+@app.route('/api/user/register', methods=['POST'])
+def create_new_user():
+    if request.method == 'POST':
+        dataIn = request.json
+        print(dataIn)
+        result = createUser(dataIn["username"])
+        if result == 'success': return "User Succesfuly Created", 201
+        #TODO: futher explanation on error 500
+        elif result == 'error': return "Could Not Create User", 500 
+
+#@app.route('/api/user/login', methods=['GET'])
+
+"""
+@app.route('/api/post/<int:post_id>', methods=['GET', 'POST', 'DELETE'])
 def post_methods(post_id):
-    if request.method == 'GET': 
-        #check if post_id exist catch error or return post info in json format
-        return
-    elif request.method == 'DELETE':
-        #function to remove post from the db
-        return f"post {post_id} succesfuly deleted", 200
+    match request.method:
+        case 'GET': return #TODO: check if post_id exist catch error or return post info in json format
+        case 'POST': return #TODO: Modify content of post
+        case 'DELETE': return f"post {post_id} succesfuly deleted", 200 #TODO: remove post from the db
+"""
+"""
+@app.route('/api/post/', methods=['GET', 'POST'])
+def post_methods():
+    match request.method:
+        case 'GET': return #TODO: return a number of post like, 5 maybe? only with id, then use GET post to have content, text etc
+        case 'POST': return #TODO: Create post
+"""
